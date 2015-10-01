ad84ae2a87ae568c7c06067926db52c68834bfad
Jvbl737 <johnnettabeverlyleefort@Johnnettas-MacBook-Pro.local>
2015-09-30 21:59:25 -0500

    Beverly-Leefort_ProblemSolving
    Problem Solving

---
+++ Week1GoToTraining/js/script.js
@@ -0,0 +1,16 @@
+/**
+ Johnnetta Beverly-Leefort
+ Week1GoToTraining
+ 9-30-2015
+
+ */
+
+//alert("Testing to see if my page work");
+console.log("Testing the console");
+
+var userPrompt1 =prompt("Please enter a Number");
+console.log (userPrompt1 );
+userPrompt1 = Number(userPrompt1);
+userPrompt1 += 6;
+
+console.log(userPrompt1 +' this is test');
