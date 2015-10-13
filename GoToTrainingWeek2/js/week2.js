fb480c2889599564b9269e9f55a19e9173628cf4
Jvbl737 <johnnettabeverlyleefort@Johnnettas-MacBook-Pro.local>
2015-10-12 23:39:03 -0500

    Starting file for week #2
    Week #2

---
+++ GoToTrainingWeek2/js/week2.js
@@ -0,0 +1,10 @@
+/*
+
+Johnnetta Beverly-Leefort
+SDI Section #3
+Go to Training Week #2
+10-12-2015
+ */
+
+
+//alert("Test to see if connection.");
--- assignment [Expression]/images/js/script.js
+++
@@ -1,6 +0,0 @@
-//Expressions
-
-
-var a = 2;
-a = a + 3 ;
-console.log (a);
