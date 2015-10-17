f7aad488eea1a4023f8ab27cd693acf58b62f530
Jvbl737 <johnnettabeverlyleefort@Johnnettas-MacBook-Pro.local>
2015-10-16 20:11:22 -0500

    Add my Expression Assignment
    expression assignment

---
+++ Beverly-Leefort_Johnnetta_Expressions_Assignment/js/expressions_Assignment.js
@@ -0,0 +1,47 @@
+/*
+ Johnnetta Beverly-Leefort
+ Expressions Assignment
+ SDI Section #3
+ 10-08-2015
+
+ */
+
+//Expressions Assignment Arithmetic Operators
+
+
+var yearBorn = 1973;
+var age = 2015 -  yearBorn;
+console.log(age);
+
+
+// Subtraction
+
+var price = 1270;
+var sales = 40 - price;
+console.log(sales);
+
+
+// Addition
+
+
+var tickets = 108;
+var taxes = 9.75 + tickets;
+console.log (taxes);
+
+//Array
+
+var price =  1270/2;
+var taxes =  40 * 18;
+var tickets = 108 + 48;
+var taxes = 9.75 + 18;
+if (price > tickets){
+    console.log(price + "is greater than" + tickets);}
+    else if (price > tickets){
+   console.log (pice + " is less than" + tickets);}
+    else{
+        console.log( price + "and" + tickets + "are equal");
+    }
+
+
+
+
--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -5,6 +5,3 @@ SDI Section #3
 Go to Training Week #2
 10-12-2015
  */
-
-
-//alert("Test to see if connection.");
